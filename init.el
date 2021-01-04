077db296434a325d7ff69bbce27b5f16a7009726
Ícaro Magalhães <icaro.lmag@gmail.com>
2021-01-03 22:25:49 -0300

    Add color scheme deeper blue

--- init.el
+++ init.el
@@ -10,6 +10,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+
+ ;; color scheme customization [IM]
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(custom-enabled-themes '(deeper-blue))
+
+ ;; plugins [IM]
  '(package-selected-packages '(cider)))
 
 (custom-set-faces
