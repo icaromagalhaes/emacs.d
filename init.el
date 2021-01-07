ef6b8f9428ac2dc909e87d97ae2231a9b1908ba1
Ícaro Magalhães <icaro.lmag@gmail.com>
2021-01-07 17:41:38 -0300

    Fix exec path from shell config

--- init.el
+++ init.el
@@ -5,21 +5,21 @@
 
 (package-initialize)
 
+;; set emacs shell to work with mac env variables [IM]
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
- ;; color scheme customization [IM]
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(custom-enabled-themes '(deeper-blue))
-
- ;; plugins [IM]
- '(package-selected-packages '(cider)))
+ '(package-selected-packages '(exec-path-from-shell cider)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
